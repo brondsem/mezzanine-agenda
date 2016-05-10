b3bd1a965454d31ffb15291b159abf68fe0cbebf
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-05-10 17:27:35 +0200

    add horizontal filter

--- mezzanine_agenda/admin.py
+++ mezzanine_agenda/admin.py
@@ -27,6 +27,7 @@ class EventAdmin(DisplayableAdmin, OwnableAdmin):
     if settings.EVENT_USE_FEATURED_IMAGE:
         list_display.insert(0, "admin_thumb")
     list_filter = deepcopy(DisplayableAdmin.list_filter) + ("location",)
+    filter_horizontal = ['blog_posts']
 
     def save_form(self, request, form, change):
         """
