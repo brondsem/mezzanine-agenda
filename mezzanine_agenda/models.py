3a5ba723573897568f1ca86eabbeeff47cde57d6
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-08-24 16:43:25 +0200

    Rm Photo class

--- mezzanine_agenda/models.py
+++ mezzanine_agenda/models.py
@@ -32,33 +32,7 @@ class SubTitle(models.Model):
         abstract = True
 
 
-class Photo(models.Model):
-    """Photo bundle with credits"""
-
-    photo = FileField(_('photo'), upload_to='images/photos', max_length=1024, blank=True, format="Image")
-    photo_credits = models.CharField(_('photo credits'), max_length=255, blank=True, null=True)
-    photo_alignment = models.CharField(_('photo alignment'), choices=ALIGNMENT_CHOICES, max_length=32, default="left", blank=True)
-    photo_description = models.TextField(_('photo description'), blank=True)
-
-    photo_card = FileField(_('card photo'), upload_to='images/photos/card', max_length=1024, blank=True, format="Image")
-    photo_card_credits = models.CharField(_('photo card credits'), max_length=255, blank=True, null=True)
-
-    photo_slider = FileField(_('slider photo'), upload_to='images/photos/slider', max_length=1024, blank=True, format="Image")
-    photo_slider_credits = models.CharField(_('photo slider credits'), max_length=255, blank=True, null=True)
-    abstract = True
-
-    class Meta:
-        abstract = True
-
-    @property
-    def card(self):
-        if self.photo_card:
-            return self.photo_card
-        else:
-            return self.photo
-
-
-class Event(Displayable, SubTitle, Ownable, Photo, RichText, AdminThumbMixin):
+class Event(Displayable, SubTitle, Ownable, RichText, AdminThumbMixin):
     """
     An event.
     """
