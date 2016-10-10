ab1f8a70502aa5f54484a40a7c86b454a1f03e70
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-10 11:30:48 +0200

    Add mentions translation

--- mezzanine_agenda/translation.py
+++ mezzanine_agenda/translation.py
@@ -6,7 +6,7 @@ from mezzanine_agenda.models import Event, EventLocation
 @register(Event)
 class EventTranslationOptions(TranslationOptions):
 
-    fields = ('title', 'sub_title', 'description', 'content')
+    fields = ('title', 'sub_title', 'description', 'content', 'mentions')
 
 @register(EventLocation)
 class EventLocationTranslationOptions(TranslationOptions):
