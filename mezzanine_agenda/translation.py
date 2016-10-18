2c6e15e6a66c507e05ab803016acd4d250f4d2dd
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-10-18 18:08:31 +0200

    Add no price comments

--- mezzanine_agenda/translation.py
+++ mezzanine_agenda/translation.py
@@ -6,7 +6,7 @@ from mezzanine_agenda.models import Event, EventLocation
 @register(Event)
 class EventTranslationOptions(TranslationOptions):
 
-    fields = ('title', 'sub_title', 'description', 'content', 'mentions')
+    fields = ('title', 'sub_title', 'description', 'content', 'mentions', 'no_price_comments')
 
 @register(EventLocation)
 class EventLocationTranslationOptions(TranslationOptions):
