d65971d892f3fc8ce0f265d97dc5c39de6d0d73a
Emilie <zawadzki@ircam.fr>
2017-04-06 15:10:44 +0200

    correcting template tags

--- mezzanine_agenda/templatetags/event_tags.py
+++ mezzanine_agenda/templatetags/event_tags.py
@@ -289,18 +289,21 @@ def subtract(value, arg):
 
 @register.filter
 def same_time_in_periods(periods):
+    is_same_time = True
     if periods:
-        last_time = periods[0].date_from.time()
+        last_time_from = periods[0].date_from.time()
+        last_time_to = None
+        if periods[0].date_to:
+            last_time_to = periods[0].date_to.time()
         for period in periods:
-            if period.date_from.time() != last_time:
-                return False
-        return True
-    return False
+            if period.date_from.time() != last_time_from:
+                is_same_time = False
+            if last_time_to and period.date_to:
+                if period.date_to.time() != last_time_to:
+                    is_same_time = False
+
+    return is_same_time
 
 @register.filter
 def tag_is_excluded(tag):
     return tag.slug in settings.EVENT_EXCLUDE_TAG_LIST
-
-@register.filter
-def period_is_more_than_hours(period, hours):
-    return (period.date_to - period.date_from).seconds > hours*3600
